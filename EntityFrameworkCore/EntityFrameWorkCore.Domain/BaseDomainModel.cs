abf6e93830e2e933abfc2d160c8613c4df868471
Alberto <albertoglezb@hotmail.com>
2024-02-08 15:03:58 -0600

    The initial models were created.

---
+++ EntityFrameworkCore/EntityFrameWorkCore.Domain/BaseDomainModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EntityFrameWorkCore.Domain;
+
+public abstract class BaseDomainModel
+{
+    public DateTime CreatedDate { get; set; }
+
+}
---
+++ EntityFrameworkCore/EntityFrameWorkCore.Domain/Coach.cs
@@ -0,0 +1,7 @@
+﻿namespace EntityFrameWorkCore.Domain;
+
+public class Coach : BaseDomainModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
