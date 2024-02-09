d6e769e2be4e84a12547f7631f48df88e91485c9
Alberto <albertoglezb@hotmail.com>
2024-02-09 11:40:55 -0600

    A query to find a single element was created.

--- EntityFrameworkCore/EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore/EntityFrameworkCore.Console/Program.cs
@@ -1,12 +1,40 @@
 ﻿using EntityFrameWorkCore.Data;
+using Microsoft.EntityFrameworkCore;
 
 // First we need a instance of context.
-var context = new FootballLeageDbContext();
+using var context = new FootballLeageDbContext();
 
 //Select all teams.
-var teams = context.Teams.ToList();
+//GetAllTeams();
 
-foreach (var team in teams)
+//Select a single record.
+GetSingleRecord();
+
+async void GetSingleRecord()
 {
-    Console.WriteLine(team.Name);
+    //First one in the list
+    var teamOne = await context.Teams.FirstAsync();//Throws an error if no data is founded.
+    var teamOneDefault = await context.Teams.FirstOrDefaultAsync();//Doesnt throw any error.
+
+    //First one in the list that meets a condition
+    var team2 = await context.Teams.FirstAsync(team => team.Id == 1);
+    var team2Default = await context.Teams.FirstOrDefaultAsync(team => team.Id == 1);
+
+    //Only one record should be returned
+    var team3 = await context.Teams.SingleAsync();
+    var team3Condition = await context.Teams.SingleAsync(team => team.Id == 2);
+    var team3ConditionDefault = await context.Teams.SingleOrDefaultAsync(team => team.Id == 2);
+
+    // Selecting based on Id
+    var team4 = await context.Teams.FindAsync(5);
+}
+
+void GetAllTeams()
+{
+    var teams = context.Teams.ToList();
+
+    foreach (var team in teams)
+    {
+        Console.WriteLine(team.Name);
+    }
 }
