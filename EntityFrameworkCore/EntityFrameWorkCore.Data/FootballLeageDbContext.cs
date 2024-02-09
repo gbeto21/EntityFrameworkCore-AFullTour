295c05836f5bb72e2784d7b9d8640a13451e5936
Alberto <albertoglezb@hotmail.com>
2024-02-09 07:21:05 -0600

    The initial set up for the DbContext class was created.

---
+++ EntityFrameworkCore/EntityFrameWorkCore.Data/FootballLeageDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameWorkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameWorkCore.Data
+{
+    public class FootballLeageDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Coach> Coaches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
