365e89abde0ef0533642d596364a9877f8f2ac9c
Alberto <albertoglezb@hotmail.com>
2024-02-09 09:37:22 -0600

    The database was seeded with a migration.

--- EntityFrameworkCore/EntityFrameWorkCore.Data/FootballLeageDbContext.cs
+++ EntityFrameworkCore/EntityFrameWorkCore.Data/FootballLeageDbContext.cs
@@ -14,5 +14,30 @@ namespace EntityFrameWorkCore.Data
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeage_EfCore; Encrypt=False");
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team
+                {
+                    Id = 1,
+                    Name = "Tivoli Gardens FC",
+                    CreatedDate = DateTimeOffset.UtcNow.DateTime
+                },
+                new Team
+                {
+                    Id = 2,
+                    Name = "Waterhouse FC",
+                    CreatedDate = DateTimeOffset.UtcNow.DateTime
+                },
+                new Team
+                {
+                    Id = 3,
+                    Name = "Humble Lions FC",
+                    CreatedDate = DateTimeOffset.UtcNow.DateTime
+                }
+            );
+
+        }
+
     }
 }
---
+++ EntityFrameworkCore/EntityFrameWorkCore.Data/Migrations/20240209153441_SeededTeams.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EntityFrameWorkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "CreatedDate", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 2, 9, 15, 34, 41, 196, DateTimeKind.Unspecified).AddTicks(1768), "Tivoli Gardens FC" },
+                    { 2, new DateTime(2024, 2, 9, 15, 34, 41, 196, DateTimeKind.Unspecified).AddTicks(1779), "Waterhouse FC" },
+                    { 3, new DateTime(2024, 2, 9, 15, 34, 41, 196, DateTimeKind.Unspecified).AddTicks(1780), "Humble Lions FC" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
